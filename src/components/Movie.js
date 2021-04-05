b99f706a5db10dae092f21a6c3c660cb0999b3d6
gguldduck111 <gguldduck111@naver.com>
2021-04-05 09:32:18 +0900

    #6 0 Getting Ready for the Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -13,7 +13,7 @@ function Movie({year, title, summary, poster, genres}){
                 )
             )}
             </ul>
-            <p className={"movie_summary"}>{summary}</p>
+            <p className={"movie_summary"}>{summary.slice(0,180)}...</p>
         </div>
     </div>
 }
